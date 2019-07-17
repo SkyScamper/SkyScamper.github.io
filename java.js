d9a178b44f51b10a0b1cede8afb9cb202918e1c5
SkyScamper <52878845+SkyScamper@users.noreply.github.com>
2019-07-16 20:30:23 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,9 +1,12 @@
-function TheFunction(){
-  var Header = document.getElementById("Header");
-  if (Header.style.display === "none") {
-    Header.style.display = "block";
-  } else {
-    Header.style.display = "none";
+var ListItems = document.getElementsByClassName("Header");
+for(var i = 0; i < ListItems.length; i++){
+  function TheFunction(){
+    var Header = document.getElementById("Header");
+    if (ListItems[i].style.display === "none") {
+      ListItems[i].style.display = "block";
+    } else {
+      ListItems[i].style.display = "none";
+    }
   }
 }
 ListItems.addEventListener("click", TheFunction);
