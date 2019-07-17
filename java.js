4bd842aacd4db5f37e30d5119b33d390addb2af5
SkyScamper <52878845+SkyScamper@users.noreply.github.com>
2019-07-16 20:44:38 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,11 +1,5 @@
-var ListItems = document.getElementsByClassName("Header");
-var Header = document.getElementById("Header");
-for(var i = 0; i < ListItems.length; i++){
-  function TheFunction(){
-    if (ListItems[i].style.display === "none") {
-      ListItems[i].style.display = "block";
-    } else {
-      ListItems[i].style.display = "none";
-    }
-  }
-Header.addEventListener("click", TheFunction);
+$(document).ready(function() {
+    $(".HeaderTog").click(function() {
+        $(this).parent().nextAll('.toggle-wrap').first().toggle('slow');
+    });
+});
